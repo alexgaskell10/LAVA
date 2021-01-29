141eb7aaf99dd3c044278a6c3cc96d547c8a7aac
Alexander Gaskell <aeg19@gpu10.doc.ic.ac.uk>
2021-01-29 11:12:46 +0000

    Ported allennlp code from LeapOfThought dir

---
+++ ruletaker/analysis.py
@@ -0,0 +1,76 @@
+import json
+import os
+import sys
+import glob
+
+class ResultsProcessor():
+    def __init__(self, dir):
+        self.dir = dir
+        self.config_path = os.path.join(dir, 'config.json')
+
+        self.load_best_metrics()
+        self.add_proof_depth()
+
+    def load_best_metrics(self, metric='validation_EM'):
+        ''' Add proof depth for each prediction
+        '''
+        metrics_paths = glob.glob(os.path.join(self.dir, 'metrics_epoch_*.json'))
+
+        self.scores = {}
+        best_score, best_idx = 0, None
+        for path in metrics_paths:
+            with open(path, 'r') as f:
+                self.scores[path] = json.load(f)
+            
+            if self.scores[path][metric] > best_score:
+                self.best_score = self.scores[path][metric]
+                self.best_idx = path
+
+    def add_proof_depth(self):
+        ''' Add proof depth for each prediction
+        '''
+        with open(self.config_path, 'r') as f:
+            config = json.load(f)
+        data_path = config['validation_data_path']
+        
+        # Extract {id: {qid: depth, ...}} pairs from original dev data
+        id2depth = {}
+        for line in open(data_path).readlines():
+            row = json.loads(line)
+            id = row.pop('id')
+            id2depth[id] = {}
+            for q in row['questions']:
+                id2depth[id][q.pop('id')] = q.pop('meta').pop('QDep')
+
+        # Add proof depth for each prediction
+        preds = self.scores[self.best_idx]['validation_predictions']
+        self.preds = []
+        for pred in preds:
+            qid = pred['id']
+            id = '-'.join(qid.split('-')[:-1])
+            if id in id2depth:
+                pred['QDep'] = id2depth[id][qid]
+                self.preds.append(pred)
+
+
+class ResultsAnalyzer(ResultsProcessor):
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.by_depth()
+
+    def by_depth(self):
+        # {depth: [#correct, #samples]}
+        scores = {i: [0, 0] for i in range(10)}
+        for pred in self.preds:
+            depth = pred['QDep']
+            scores[depth][0] += pred['is_correct']
+            scores[depth][1] += 1
+            if depth == 1 and not pred['is_correct']:
+                pass
+
+        [print(k,v[1],v[0]/v[1]) for k,v in scores.items() if v[1] > 0]
+        print(sum([v[0] for k,v in scores.items() if k <= 5]) / sum([v[1] for k,v in scores.items() if k <= 5]))
+
+
+if __name__ == '__main__':
+    ResultsAnalyzer('runs/t12')
